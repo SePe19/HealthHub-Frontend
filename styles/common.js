64767380405fcf249f64d275a16decfdd7ca6382
Felix Bryld <felixbryld@MBPtilhndeFelix.home>
2024-05-28 09:31:28 +0200

    common styles

---
+++ styles/common.js
@@ -0,0 +1,26 @@
+// common style values
+
+const common = {
+    gradients: {
+        primaryGradient: ['#6CEC34', '#34EC94'] // primary gradient
+    },
+    // gradient colors
+    primary: '#6CEC34',     // Primary color
+    secondary: '#34EC94',   // Secondary color
+    // main colors
+    mainColor: '#45EC77', // for elements where it doesnt make sense to use a gradient
+    secondaryColor: '#D2D2D2',
+    // other colors
+    gold: '#FAC170',
+    darkGrey: '#333232',
+    grey: '#4B4B4B',
+    lightGrey: '#5C5C5C',
+    background: '#3F3F3F',
+    white: '#FFFFFF',
+    remove: '#F14949',
+    // text colors
+    textPrimary: '#FFFFFF', // Primary text color
+    textSecondary: '#333232', // Secondary text color
+};
+
+export default common;
