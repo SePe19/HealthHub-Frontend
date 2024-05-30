0dc298d757138fc428276cd718a7c044336b2496
Felix Bryld <felixbryld@MBPtilhndeFelix.home>
2024-05-30 09:35:52 +0200

    removed depricated imports

--- App.js
+++ App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-//import { createStackNavigator } from '@react-navigation/stack';
-//import Workouts from './pages/Workouts';
-//import CreateWorkout from './pages/CreateWorkout';
 import TabNavigator from './components/navigation/TabNavigator';
 
 const App = () => {
